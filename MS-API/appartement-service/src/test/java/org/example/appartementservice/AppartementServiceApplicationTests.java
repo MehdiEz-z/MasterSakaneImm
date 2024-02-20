0d4fcb80a275858379530839310ca064e028ae00
EZ-ZAHRI <m.ezzahri@itroadgroup.com>
2024-02-20 10:13:53 +0100

    Mise en place de l'architecture microservices pour le projet Master Sakane Immobilier partie backend

---
+++ MS-API/appartement-service/src/test/java/org/example/appartementservice/AppartementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.appartementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppartementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
