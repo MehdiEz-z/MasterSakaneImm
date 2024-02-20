2ab11acad43e80d0e3e031b0751b6cfdf2c19862
EZ-ZAHRI <m.ezzahri@itroadgroup.com>
2024-02-20 16:18:17 +0100

    Test de creation de residence avec implementation de la methode Create residence

--- MS-API/appartement-service/src/main/java/org/example/appartementservice/services/inmplementations/ResidenceServiceImpl.java
+++ MS-API/appartement-service/src/main/java/org/example/appartementservice/services/inmplementations/ResidenceServiceImpl.java
@@ -1,9 +1,9 @@
 package org.example.appartementservice.services.inmplementations;
+import org.example.appartementservice.handlers.exceptionHandler.OperationsException;
 import org.example.appartementservice.models.entities.Residence;
 import org.example.appartementservice.repositories.ResidenceRepository;
 import org.example.appartementservice.services.interfaces.ResidenceService;
 import org.springframework.stereotype.Component;
-
 import java.util.Collections;
 import java.util.List;
 @Component
@@ -14,10 +14,15 @@ public class ResidenceServiceImpl implements ResidenceService{
     }
     @Override
     public Residence createResidence(Residence residence) {
-        return null;
+        String reference = generateReferenceResidence(residence);
+        residence.setReference(reference);
+        return residenceRepository.save(residence);
     }
     public String generateReferenceResidence(Residence residence) {
         long count = residenceRepository.count();
+        if (count < 0) {
+            throw new OperationsException("Erreur lors de la génération de la référence de la résidence");
+        }
         String reference = "RES-" + (count + 1);
         if (residence.getReference() == null || residence.getReference().isEmpty()) {
             residence.setReference(reference);
--- MS-API/appartement-service/src/test/java/org/example/appartementservice/services/residences/ResidenceServiceImplTest.java
+++ MS-API/appartement-service/src/test/java/org/example/appartementservice/services/residences/ResidenceServiceImplTest.java
@@ -1,4 +1,5 @@
 package org.example.appartementservice.services.residences;
+import org.example.appartementservice.handlers.exceptionHandler.OperationsException;
 import org.example.appartementservice.models.entities.Residence;
 import org.example.appartementservice.repositories.ResidenceRepository;
 import org.example.appartementservice.services.inmplementations.ResidenceServiceImpl;
@@ -45,5 +46,31 @@ class ResidenceServiceImplTest {
         String reference = residenceService.generateReferenceResidence(residence2);
         assertNotNull(reference);
         assertEquals("RES-2", reference);
+    }
+    @Test
+    void testGenerateReferenceResidenceFailAndThrowException(){
+        Residence residence2 = Residence.builder()
+                .nom("Residence 2")
+                .description("Description 2")
+                .adresse("Adresse 2")
+                .build();
+        Mockito.when(residenceRepository.count()).thenReturn((long) -1);
+        OperationsException exception = assertThrows(OperationsException.class,
+                () -> residenceService.generateReferenceResidence(residence2));
+        String expectedMessage = "Erreur lors de la génération de la référence de la résidence";
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+    @Test
+    void testCreateResidenceSuccess(){
+        Residence residence1 = createResidence();
+        Residence residence2 = Residence.builder()
+                .nom("Residence 1")
+                .description("Description 1")
+                .adresse("Adresse 1")
+                .build();
+        Mockito.when(residenceRepository.save(residence2)).thenReturn(residence2);
+        Residence createdResidence = residenceService.createResidence(residence2);
+        assertNotNull(createdResidence);
+        assertEquals(residence1.getReference(), residence2.getReference());
     }
 }
