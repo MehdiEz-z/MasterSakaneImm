b073a54db77813e5ff14651a70399e3c49c715da
EZ-ZAHRI <m.ezzahri@itroadgroup.com>
2024-02-21 12:06:09 +0100

    Finalisation des tests afin de commencer l'implementation des methodes du service immeuble a partir des test realisé

--- MS-API/appartement-service/src/main/java/org/example/appartementservice/repositories/ImmeubleRepository.java
+++ MS-API/appartement-service/src/main/java/org/example/appartementservice/repositories/ImmeubleRepository.java
@@ -2,8 +2,12 @@ package org.example.appartementservice.repositories;
 import org.example.appartementservice.models.entities.Immeuble;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface ImmeubleRepository extends JpaRepository<Immeuble, String> {
     boolean existsByReference(String reference);
     Immeuble findByReferenceAndResidence_Reference(String reference, String referenceResidence);
+    List<Immeuble> findAllByResidence_Reference(String reference);
 }
--- MS-API/appartement-service/src/test/java/org/example/appartementservice/services/immeubles/ImmeubleServiceImplTest.java
+++ MS-API/appartement-service/src/test/java/org/example/appartementservice/services/immeubles/ImmeubleServiceImplTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImmeubleServiceImplTest {
@@ -54,12 +56,27 @@ class ImmeubleServiceImplTest {
         assertEquals("RES-1-IMM-2", reference);
     }
     @Test
+    void testCreateImmeubleNotExistingResidence(){
+        Immeuble immeuble2 = Immeuble.builder()
+                .numero("Immeuble-1")
+                .residence(Residence.builder().reference("RES-1").build())
+                .build();
+        Mockito.when(residenceService.getResidenceByReference(immeuble2.getResidence().getReference()))
+                .thenReturn(null);
+        String exceptedMessage = "La Residence \"" + immeuble2.getResidence().getReference() + "\" n'existe pas";
+        Exception exception = assertThrows(ResourcesNotFoundException.class,
+                () -> immeubleService.createImmeuble(immeuble2));
+        assertEquals(exceptedMessage, exception.getMessage());
+    }
+    @Test
     void testCreateImmeubleSuccess(){
         Immeuble immeuble1 = createImmeuble();
         Immeuble immeuble2 = Immeuble.builder()
                 .numero("Immeuble-1")
                 .residence(Residence.builder().reference("RES-1").build())
                 .build();
+        Mockito.when(residenceService.getResidenceByReference(immeuble2.getResidence().getReference()))
+                .thenReturn(immeuble2.getResidence());
         Mockito.when(immeubleRepository.save(immeuble2)).thenReturn(immeuble2);
         Immeuble createdImmeuble = immeubleService.createImmeuble(immeuble2);
         assertNotNull(createdImmeuble);
@@ -101,5 +118,24 @@ class ImmeubleServiceImplTest {
         Immeuble immeuble = immeubleService.getImmeubleByReferenceAndResidence(immeuble1.getReference(), immeuble1.getResidence().getReference());
         assertNotNull(immeuble);
         assertEquals(immeuble1.getReference(), immeuble.getReference());
+    }
+    @Test
+    void testGetAllImmeubleByResidence(){
+        Immeuble immeuble1 = createImmeuble();
+        Immeuble immeuble2 = Immeuble.builder()
+                .reference("RES-1-IMM-2")
+                .numero("Immeuble-2")
+                .residence(Residence.builder().reference("RES-1").build())
+                .build();
+        Immeuble immeuble3 = Immeuble.builder()
+                .reference("RES-1-IMM-3")
+                .numero("Immeuble-3")
+                .residence(Residence.builder().reference("RES-1").build())
+                .build();
+        Mockito.when(immeubleRepository.findAllByResidence_Reference("RES-1"))
+                .thenReturn(List.of(immeuble1, immeuble2, immeuble3));
+        List<Immeuble> immeubles = immeubleService.getAllImmeubleByResidence();
+        assertNotNull(immeubles);
+        assertEquals(3, immeubles.size());
     }
 }
